91a482452efe256caf30b97113f22dc9d406be80
jhiebunda <jhie.bunda@gmail.com>
2021-04-12 13:43:57 +0800

    tracker page.

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -5,5 +5,33 @@
 @endsection
 
 @section('content')
-
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-category">Vehicle List</h5>
+                    <h4 class="card-title">Registered Vehicles</h4>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead class="table">
+                                <th>Name</th>
+                                <th>Type</th>
+                                <th>Color</th>
+                                <th>Plate No.</th>
+                                <th>Destination</th>
+                                <th>Tracker ID</th>
+                                <th>Date</th>
+                                <th>Time-In</th>
+                                <th>Time-Out</th>
+                                <th>Duration</th>
+                                <th>Map</th>
+                                <th>ACTIONS</th>
+                            </thead>
+                            
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
 @endsection
