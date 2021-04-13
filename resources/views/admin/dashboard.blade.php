487930a6a66e151cd3ee08e0853d9f3aa7f60efa
jhiebunda <jhie.bunda@gmail.com>
2021-04-13 21:11:29 +0800

    commit

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,7 +6,44 @@
 
 @section('content')
 <div class="row">
-          <div class="col-md-12">
+          <div class="col-lg-6">
+            <div class="card card-chart">
+                <div class="card-header">
+                    <h5 class="card-category">Statistics</h5>
+                    <h4 class="card-title">Private</h4>
+                </div>
+                <div class="card-body">
+                    <div class="chart-area">
+                        <canvas id="lineChartExampleWithNumbersAndGrid"></canvas>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div class="stats">
+                        <i class="now-ui-icons arrows-1_refresh-69"></i> Just Updated
+                    </div>
+                </div>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="card card-chart">
+                <div class="card-header">
+                    <h5 class="card-category">Statistics</h5>
+                    <h4 class="card-title">Delivery</h4>
+                </div>
+                <div class="card-body">
+                    <div class="chart-area">
+                        <canvas id="lineChartExampleWithNumbersAndGrid"></canvas>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div class="stats">
+                        <i class="now-ui-icons arrows-1_refresh-69"></i> Just Updated
+                    </div>
+                </div>
+            </div>
+          </div>
+
+          <!-- <div class="col-md-12">
             <div class="card">
               <div class="card-header">
                 <h4 class="card-title"> Simple Table</h4>
@@ -32,7 +69,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 </div>
 
 
--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -6,11 +6,11 @@
 
 @section('content')
     <div class="row">
-        <div class="col-lg-4">
+    <div class="col-lg-6">
             <div class="card card-chart">
                 <div class="card-header">
-                    <h5 class="card-category">Global Sales</h5>
-                    <h4 class="card-title">Shipped Products</h4>
+                    <h5 class="card-category">Statistics</h5>
+                    <h4 class="card-title">Private</h4>
                 </div>
                 <div class="card-body">
                     <div class="chart-area">
@@ -23,7 +23,25 @@
                     </div>
                 </div>
             </div>
-        </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="card card-chart">
+                <div class="card-header">
+                    <h5 class="card-category">Statistics</h5>
+                    <h4 class="card-title">Delivery</h4>
+                </div>
+                <div class="card-body">
+                    <div class="chart-area">
+                        <canvas id="lineChartExampleWithNumbersAndGrid"></canvas>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div class="stats">
+                        <i class="now-ui-icons arrows-1_refresh-69"></i> Just Updated
+                    </div>
+                </div>
+            </div>
+          </div>
         <!-- <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
