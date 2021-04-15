5b006fd540dc4c1fc64516cb65ae03364f4ebe62
jhiebunda <jhie.bunda@gmail.com>
2021-04-16 00:07:05 +0800

    update

--- app/Http/Controllers/Admin/AdminreportController.php
+++ app/Http/Controllers/Admin/AdminreportController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Vehicle;
 
 class AdminreportController extends Controller
 {
     public function adminreport() {
-        return view('admin.adminreport');
+        $data = Vehicle::latest()->paginate(5);
+
+        return view('admin.adminreport', compact('data'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
     }
 }
